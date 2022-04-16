7de5f4e74cf3cadaab7074757fc47c4aa0bd1e7f
Kaushik Bhadra <kbhadra100@gmail.com>
2022-04-16 17:45:15 +0530

    setup for production

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
 
 import './styles/style.scss'
 import 'normalize.css/normalize.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,58 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  entry: ['./src/app.js'],
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/, // Check all extension, when run babel
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env) => {
+  let isProduction
+  if (env.production === true) {
+    isProduction = true
+  } else if (env.production === undefined) isProduction = false
+
+  return {
+    entry: ['./src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
     },
-    historyApiFallback: true,
-  },
+    mode: isProduction ? 'production' : 'development',
+
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/, // Check all extension, when run babel
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      historyApiFallback: true,
+    },
+  }
 }
